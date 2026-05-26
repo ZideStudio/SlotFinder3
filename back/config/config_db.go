b914e908e5ba00443c3867044729ddee2d69d7ac
Nekeo <nekeo@zide.fr>
2026-05-26 19:25:11 +0200

    7

--- back/config/config_db.go
+++ back/config/config_db.go
@@ -12,6 +12,7 @@ type DbConfiguration struct {
 	User     string `env:"DB_USER"`
 	Password string `env:"DB_PASSWORD"`
 	Name     string `env:"DB_NAME"`
+	TimeZone string `env:"DB_TIMEZONE"`
 }
 
 // Dialect returns "postgres"
@@ -21,14 +22,19 @@ func (c DbConfiguration) Dialect() string {
 
 // GetPostgresConnectionInfo returns Postgres URL string
 func (c DbConfiguration) GetPostgresConnectionInfo() string {
+	timezone := c.TimeZone
+	if timezone == "" {
+		timezone = "UTC" // Default to UTC if not specified
+	}
+
 	if c.Password == "" {
 		return fmt.Sprintf(
-			"host=%s port=%d user=%s dbname=%s sslmode=disable",
-			c.Host, c.Port, c.User, c.Name)
+			"host=%s port=%d user=%s dbname=%s sslmode=disable TimeZone=%s",
+			c.Host, c.Port, c.User, c.Name, timezone)
 	}
 	return fmt.Sprintf(
 		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable TimeZone=%s",
-		c.Host, c.Port, c.User, c.Password, c.Name, "Europe/Paris")
+		c.Host, c.Port, c.User, c.Password, c.Name, timezone)
 }
 
 // GetPostgresConfig returns PostgresConfig object
@@ -44,5 +50,6 @@ func GetPostgresConfig() DbConfiguration {
 		User:     os.Getenv("DB_USER"),
 		Password: os.Getenv("DB_PASSWORD"),
 		Name:     os.Getenv("DB_NAME"),
+		TimeZone: os.Getenv("DB_TIMEZONE"),
 	}
 }
