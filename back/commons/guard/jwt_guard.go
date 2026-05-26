c0d5314fb913c565fe730ace75dd1f52b08ed949
Nekeo <nekeo@zide.fr>
2026-05-26 21:23:48 +0200

    16

--- back/commons/guard/jwt_guard.go
+++ back/commons/guard/jwt_guard.go
@@ -5,7 +5,6 @@ import (
 	"app/commons/helpers"
 	"app/commons/lib"
 	"app/config"
-	"encoding/base64"
 	"errors"
 	"net/http"
 	"time"
@@ -45,23 +44,9 @@ func ParseToken(jwtToken string) (*Claims, error) {
 
 	config := config.GetConfig()
 
-	// Try to read from base64 environment variable first
-	publicKeyB64 := os.Getenv("AUTH_PUBLIC_KEY_B64")
-	var f []byte
-	var err error
-
-	if publicKeyB64 != "" {
-		// Decode from base64
-		f, err = base64.StdEncoding.DecodeString(publicKeyB64)
-		if err != nil {
-			return claims, err
-		}
-	} else {
-		// Fallback to file path (for backwards compatibility)
-		f, err = os.ReadFile(config.Auth.PublicPemPath)
-		if err != nil {
-			return claims, err
-		}
+	f, err := os.ReadFile(config.Auth.PublicPemPath)
+	if err != nil {
+		return claims, err
 	}
 
 	_, err = jwt.ParseWithClaims(jwtToken, claims, func(token *jwt.Token) (any, error) {
@@ -78,23 +63,9 @@ func ParseToken(jwtToken string) (*Claims, error) {
 func GenerateAccessToken(claims *Claims) (string, error) {
 	config := config.GetConfig()
 
-	// Try to read from base64 environment variable first
-	privateKeyB64 := os.Getenv("AUTH_PRIVATE_KEY_B64")
-	var privateKeyFile []byte
-	var err error
-
-	if privateKeyB64 != "" {
-		// Decode from base64
-		privateKeyFile, err = base64.StdEncoding.DecodeString(privateKeyB64)
-		if err != nil {
-			return "", err
-		}
-	} else {
-		// Fallback to file path (for backwards compatibility)
-		privateKeyFile, err = os.ReadFile(config.Auth.PrivatePemPath)
-		if err != nil {
-			return "", err
-		}
+	privateKeyFile, err := os.ReadFile(config.Auth.PrivatePemPath)
+	if err != nil {
+		return "", err
 	}
 
 	privateKey, err := jwt.ParseRSAPrivateKeyFromPEM(privateKeyFile)
